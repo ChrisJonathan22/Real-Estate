5055fc446ae10576867c5a5b0c4800fd972964f8
Chris Jonathan <christophereko@hotmail.fr>
2019-12-27 23:09:44 +0000

    I've created some components.

--- root/frontend/src/App.js
+++ root/frontend/src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+// import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          {/* <Route exact path='/' component={Home} /> */}
+          {/* <Route exact path = '/properties' component = { Properties } /> */}
+          {/* <Route exact path = '/property' component = { Property } /> */}
+        </Switch>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
