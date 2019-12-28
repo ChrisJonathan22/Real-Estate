67f97fb217eefc7694a2531941367d64814c0a74
Chris Jonathan <christophereko@hotmail.fr>
2019-12-28 14:13:38 +0000

    I'm working on the menu.

--- root/frontend/src/components/Navbar/Navbar.js
+++ root/frontend/src/components/Navbar/Navbar.js
@@ -1,34 +1,28 @@
   
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo  from '../../Assets/logo.png';
 import './Navbar.scss';
 
 const Navbar =  () => {
   return (
           <div>
-              <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                  <p className='logo' data-toggle="collapse" data-target=".navbar-collapse.show"><Link className="navbar-brand" exact to = '/'>Real Estate</Link></p>
-                  <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                  </button>
-                  <div className="navbar-collapse collapse" id="navbarColor01">
-                    <ul className="navbar-nav mr-auto">
-                      <li className="nav-item active navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse.show" aria-controls="navbarColor01" aria-expanded="true" aria-label="Toggle navigation">
-                        <Link className="nav-link" exact to = '/'>Home<span className="sr-only">(current)</span></Link>
-                      </li>
-                      <li className="nav-item navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse.show" aria-controls="navbarColor01" aria-expanded="true" aria-label="Toggle navigation">
-                        <Link className="nav-link" to = '/recipes'>Recipes</Link>
-                      </li>
-                      <li className="nav-item navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse.show" aria-controls="navbarColor01" aria-expanded="true" aria-label="Toggle navigation">
-                        <Link className="nav-link" to = '/about'>About</Link>
-                      </li>
-                      <li className="nav-item navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse.show" aria-controls="navbarColor01" aria-expanded="true" aria-label="Toggle navigation">
-                        <Link className="nav-link" to = '/contact'>Contact</Link>
-                      </li>
-                    </ul>
-                  </div>
+              <nav className="navbar navbar-expand-x1 navbar-light @@classList">
+                <div className="container">
+                  <ul className="left">
+                    <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/">Sales</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/">Lettings</a></li>
+                  </ul>
+                  {/* <a className="navbar-brand mx-auto" href="/" >Real Estate</a> */}
+                  <a className="navbar-brand mx-auto" href="/" ><img src={ Logo } /></a>
+                  <ul className="right">
+                    <li className="nav-item"><a className="nav-link" href="/">New Homes</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/">Contact</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/">Useful Info</a></li>
+                  </ul>
+                </div>
               </nav>
-              <div className='nav-clear'></div>
           </div>
   );
 }
