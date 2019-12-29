253b173ecef54105337024735d480eaf00fba5ea
Chris Jonathan <christophereko@hotmail.fr>
2019-12-29 14:57:29 +0000

    I've done some work on the menu.

--- root/frontend/src/components/Navbar/Navbar.js
+++ root/frontend/src/components/Navbar/Navbar.js
@@ -10,16 +10,16 @@ const Navbar =  () => {
               <nav className="navbar navbar-expand-x1 navbar-light @@classList">
                 <div className="container">
                   <ul className="left">
-                    <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
-                    <li className="nav-item"><a className="nav-link" href="/">Sales</a></li>
-                    <li className="nav-item"><a className="nav-link" href="/">Lettings</a></li>
+                    <li className="nav-item"><Link className="nav-link" exact to="/">Home</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/sales">Sales</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/lettings">Lettings</Link></li>
                   </ul>
                   {/* <a className="navbar-brand mx-auto" href="/" >Real Estate</a> */}
-                  <a className="navbar-brand mx-auto" href="/" ><img src={ Logo } /></a>
+                  <Link className="navbar-brand mx-auto" exact to="/" ><img alt="logo" src={ Logo } /></Link>
                   <ul className="right">
-                    <li className="nav-item"><a className="nav-link" href="/">New Homes</a></li>
-                    <li className="nav-item"><a className="nav-link" href="/">Contact</a></li>
-                    <li className="nav-item"><a className="nav-link" href="/">Useful Info</a></li>
+                    <li className="nav-item"><Link className="nav-link" to="/new-homes">New Homes</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/info">Useful Info</Link></li>
                   </ul>
                 </div>
               </nav>
