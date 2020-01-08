08eff16003194595fa77daa20d35fca2917feffd
Chris Jonathan <christophereko@hotmail.fr>
2020-01-08 23:21:08 +0000

    I've added some content to the schema.

--- root/backend/schema.js
+++ root/backend/schema.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
 let propertySchema = new mongoose.Schema({ 
-    
+    image: String,
+    price: Number,
+    bedroom: Number,
+    location: String,
+    propertyType: String,
+    contractType: String
 });
