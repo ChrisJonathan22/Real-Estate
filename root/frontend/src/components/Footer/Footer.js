1fa9bf478c43d4575c5fae6e94ac9e6a5cb93e1b
Chris Jonathan <christophereko@hotmail.fr>
2020-06-14 22:40:11 +0100

    I've added social media icons to the footer and addresses.

--- root/frontend/src/components/Footer/Footer.js
+++ root/frontend/src/components/Footer/Footer.js
@@ -11,6 +11,41 @@ export default function Footer () {
                         <div className="logo__container">
                             <a href="/"><img alt="logo" src={logo} /></a>
                         </div>
+                        <div className="social__container">
+                            <div className="icons">
+                                <a href="#" className="fa fa-facebook"></a>
+                            </div>
+
+                            <div className="icons">
+                                <a href="#" className="fa fa-linkedin"></a>
+                            </div>
+                                
+                            <div className="icons">
+                                <a href="#" className="fa fa-twitter"></a>
+                            </div>
+                        </div>
+
+                        <div className="address__container r__container">
+                            <div className="left__address">
+                                <h5>Gateshead Office</h5>
+                                <p>Real Estate</p>
+                                <p>1 Long Street</p>
+                                <p>Gateshead</p>
+                                <p>N11 1XX</p>
+                                <p>Tel: <a href="tel:02056786380">020 5678 6380</a></p>
+                                <p>Email: <a href="mailto:info-gateshead@realestate.co.uk">info-gateshead@realestate.co.uk</a></p>
+                            </div>
+
+                            <div className="left__address">
+                                <h5>Newcastle Office</h5>
+                                <p>Real Estate</p>
+                                <p>1 Short Street</p>
+                                <p>Newcastle</p>
+                                <p>N20 2XX</p>
+                                <p>Tel: <a href="tel:02040786320">020 4078 6320</a></p>
+                                <p>Email: <a href="mailto:info-newcastle@realestate.co.uk">info-newcastle@realestate.co.uk</a></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="bottom__footer">
