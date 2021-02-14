f39f5c7653ed8955d37cd42df7dd3dc75fda1a53
Chris Jonathan <christophereko@hotmail.fr>
2021-02-14 12:44:26 +0000

    I've migrated the DB to MongoDB Atlas.

--- root/backend/db.js
+++ root/backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://chris:chris22@ds135335.mlab.com:35335/realestate', { useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://chris:chris22@realestate.7fgke.mongodb.net/realestate?retryWrites=true&w=majority', { useUnifiedTopology: true });
 
 const connect = mongoose.connection;   
 
